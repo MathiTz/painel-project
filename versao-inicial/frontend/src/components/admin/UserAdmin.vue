b0b5e3f45e634b4c3029a0ea8f235ae843ea27cb
Mathitz <matheusalves789@gmail.com>
2019-06-16 17:19:48 -0300

    Componente Usuário

---
+++ versao-inicial/frontend/src/components/admin/UserAdmin.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="user-admin">
+    <h1>Usuários Componente</h1>
+  </div>
+</template>
+
+<script>
+export default { name: "UserAdmin" };
+</script>
+
+<style>
+</style>
--- versao-inicial/frontend/src/main.js
+++ versao-inicial/frontend/src/main.js
@@ -5,10 +5,16 @@ import App from "./App";
 
 import "./config/bootstrap";
 import store from "./config/store";
+import router from "./config/router";
 
 Vue.config.productionTip = false;
 
+// TEMPORÁRIO !
+require("axios").defaults.headers.common["Authorization"] =
+  "bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6Ik1hdGhldXMgQWx2ZXMiLCJlbWFpbCI6Im1hdGhldXMuc2FudG9zQGFsdS51ZmMuYnIiLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNTYwNzE0OTkxLCJleHAiOjE1NjA5NzQxOTF9.y3Eo10IqTRoxWphJZ8LNzqVmQWWPez0TWe3OmB2XPMI";
+
 new Vue({
   store,
+  router,
   render: h => h(App)
 }).$mount("#app");
