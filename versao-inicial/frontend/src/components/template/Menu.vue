90a478125f73be3d2d66eb74b18f84e305f3fa9f
Mathitz <matheusalves789@gmail.com>
2019-06-16 13:45:08 -0300

    Save menu

--- versao-inicial/frontend/src/components/template/Menu.vue
+++ versao-inicial/frontend/src/components/template/Menu.vue
@@ -12,5 +12,9 @@ export default {
 .menu {
   grid-area: menu;
   background: linear-gradient(to right, #232526, #414345);
+
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
 }
 </style>
